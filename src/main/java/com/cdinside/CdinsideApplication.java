57e9fafc29b74c6069b48a778c3334c64aff239c
전민오 <jbj338033@gmail.com>
2025-06-25 14:01:55 +0900

    init

---
+++ src/main/java/com/cdinside/CdinsideApplication.java
@@ -0,0 +1,13 @@
+package com.cdinside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdinsideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdinsideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cdinside/CdinsideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdinside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdinsideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
